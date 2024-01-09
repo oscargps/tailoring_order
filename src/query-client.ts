61011da4c52f53961487ae52d64ae87395765c82
Oscar Pastrana <opastra@bancodebogota.com.co>
2024-01-09 18:47:37 -0500

    ➕ ADD DEPENDENCIES

---
+++ src/query-client.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient();
+
+export default queryClient;
